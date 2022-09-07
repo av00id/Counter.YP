e5380ad42c4c2687503d5c00d7fd3bb641ef2bb0
av00id <112687198+av00id@users.noreply.github.com>
2022-09-07 21:51:10 +0300

    Исправлены отступы у  MARK - комментариев. Убран лишний вызов функции updateCounter.

--- Counter.YP/ViewController.swift
+++ Counter.YP/ViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-//MARK: - Переменная отображения значения счетчика
+    //MARK: - Переменная отображения значения счетчика
     
     @IBOutlet private weak var countLabel: UILabel!
     
-//MARK: - Переменная отображения кнопки для изменения значения счетчика
+    //MARK: - Переменная отображения кнопки для изменения значения счетчика
     
     @IBOutlet private weak var countButton: UIButton!
-//MARK: - Переменная значения счетчика
+    //MARK: - Переменная значения счетчика
     
    private var count: UInt = 0 {
         didSet {
@@ -31,17 +31,17 @@ class ViewController: UIViewController {
         countButton.layer.cornerRadius = 20
         countButton.layer.masksToBounds = true //закругление углов кнопки
     }
-//MARK: - Функция обновления текста в лейбле
+    //MARK: - Функция обновления текста в лейбле
     
     private func updateCounter() {
         countLabel.text = "Значение счетчика:  \(count)"
+        print("Вызов функции")
     }
 
-//MARK: - Работа кнопки для изменения значения счетчика
+    //MARK: - Работа кнопки для изменения значения счетчика
     
     @IBAction private func tapForUpdateCount(_ sender: Any) {
         count += 1
-        updateCounter()
         print("Значение счетчика:  \(count)")
               }
     
