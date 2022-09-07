ad1355ff50cf51c37bc071550f8e7a93bb452781
av00id <112687198+av00id@users.noreply.github.com>
2022-09-07 14:39:10 +0300

    Создание приложения-счетчика Яндекс-Практикум

--- Counter.YP/ViewController.swift
+++ Counter.YP/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//MARK: - Переменная отображения значения счетчика
+    
+    @IBOutlet private weak var countLabel: UILabel!
+    
+//MARK: - Переменная отображения кнопки для изменения значения счетчика
+    
+    @IBOutlet private weak var countButton: UIButton!
+//MARK: - Переменная значения счетчика
+    
+   private var count: UInt = 0 {
+        didSet {
+            updateCounter()
+        } /// при каждом обновлении значения переменной обновляется текст в лейбле
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        countButton.layer.cornerRadius = 20
+        countButton.layer.masksToBounds = true //закругление углов кнопки
+    }
+//MARK: - Функция обновления текста в лейбле
+    
+    private func updateCounter() {
+        countLabel.text = "Значение счетчика:  \(count)"
     }
 
-
+//MARK: - Работа кнопки для изменения значения счетчика
+    
+    @IBAction private func tapForUpdateCount(_ sender: Any) {
+        count += 1
+        updateCounter()
+        print("Значение счетчика:  \(count)")
+              }
+    
 }
 
